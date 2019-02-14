7f5dd801dbef60df3bafae7698977a8cebd612c7
qiubee <45637654+qiubee@users.noreply.github.com>
2019-02-14 18:02:53 +0100

    Update l3-a-vector-type

--- l3/l3-a-vector-type.js
+++ l3/l3-a-vector-type.js
@@ -3,12 +3,31 @@ class Vec {
         this.x = x;
         this.y = y;
     }
+
+    get length() {
+        return Math.sqrt((this.x ** 2) + (this.y ** 2));
+    }
 }
 
-Vec.prototype.plus = function() {
+Vec.prototype.plus = function (vector) {
+    let x = this.x + vector.x;
+    let y = this.y + vector.y;
+    return new Vec(x, y);
 
 }
 
-Vec.prototype.minus = function() {
-
+Vec.prototype.minus = function (vector) {
+    let x = this.x - vector.x;
+    let y = this.y - vector.y;
+    return new Vec(x, y);
 }
+
+
+let vec1 = new Vec(5, 6);
+let vec2 = new Vec(15, 35);
+
+console.log(vec1.plus(vec2));
+console.log(vec2.minus(vec1));
+
+console.log(vec1.length);
+console.log(vec2.length);
